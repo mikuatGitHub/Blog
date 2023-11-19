1cc0a7be577fd837257d3613d44c035a676c6831
miku <128950322+mikuatGitHub@users.noreply.github.com>
2023-11-19 19:19:30 +0900

    try-vercel-cache-clear2

--- app/api/revalidate/route.js
+++ app/api/revalidate/route.js
@@ -1,37 +1,20 @@
 import { NextResponse } from "next/server";
-// import { revalidatePath } from "next/cache";
+import { revalidatePath } from "next/cache";
 
 export function GET(request) {
-  return NextResponse.json(
-    {
-      timestamp: Date.now(),
-    },
-    {
-      status: 200,
-      headers: {
-        "Cache-Control": "no-store",
-        "CDN-Cache-Control": "no-store",
-        "Vercel-CDN-Cache-Control": "no-store",
-      },
-    }
-  );
+  const { searchParams } = new URL(request.url);
+  const secret = searchParams.get("secret");
+  if (secret !== process.env.SECRET_TOKEN) {
+    return NextResponse.json({ message: "Invalid token" }, { status: 401 });
+  }
 
-  // const { searchParams } = new URL(request.url);
-  // const secret = searchParams.get("secret");
-  // if (secret !== process.env.SECRET_TOKEN) {
-  //   return NextResponse.json({ message: "Invalid token" }, { status: 401 });
-  // }
-
-  // try {
-  //   revalidatePath("/blog/[slug]");
-  //   return NextResponse.json({ revalidated: false, now: Date.now() });
-  // } catch (err) {
-  //   return NextResponse.json(
-  //     { message: "Error revalidating" },
-  //     { status: 500 }
-  //   );
-  // }
+  try {
+    revalidatePath("/blog/[slug]");
+    return NextResponse.json({ revalidated: false, now: Date.now() });
+  } catch (err) {
+    return NextResponse.json(
+      { message: "Error revalidating" },
+      { status: 500 }
+    );
+  }
 }
-
-//vercel cache clear
-export const runtime = 'edge';
